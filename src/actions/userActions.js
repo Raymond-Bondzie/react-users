d54f3cec279a298228c734e6812a55915bd61676
Raymond Bondzie <raymond.bondzie.2@gmail.com>
2021-11-10 12:35:52 +0000

    added delete/update on firebase

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -2,14 +2,20 @@
 
 
 export const addUser = (user) => {
-    return(dispatch, state, {getFirestore}) => {
-        getFirestore()
-        .collection("users")
-        .add(user)
-        .then(() =>{
+    return (dispatch, state, { getFirestore }) => {
+		getFirestore()
+			.collection("users")
+			.add({ ...user, timestamp: getFirestore().FieldValue.serverTimestamp() })
+			.then(() => {});
+	};
+    // return(dispatch, state, {getFirestore}) => {
+    //     getFirestore()
+    //     .collection("users")
+    //     .add(user)
+    //     .then(() =>{
 
-        })
-    }
+    //     })
+    // }
     // return{
     //     type: "ADD_USER",
     //     payload: newUser,
@@ -18,29 +24,44 @@ export const addUser = (user) => {
 
 
 export const deleteUser = (userId) => {
-    return{
-        type: "DELETE_USER",
-        payload: userId
-    }
+    return (dispatch, state, { getFirestore }) => {
+		getFirestore()
+			.collection("users")
+			.doc(userId)
+			.delete()
+			.then(() => {});
+	};
+    // return{
+    //     type: "DELETE_USER",
+    //     payload: userId
+    // }
 };
 
 
 export const editUser = (userId, editedUser) => {
-    return {
-        type: "EDIT_USER",
-        payload: {userId , editedUser}
-    }
+    return (dispatch, state, { getFirestore }) => {
+		getFirestore()
+			.collection("users")
+			.doc(userId)
+			.update(editedUser)
+			.then(() => {});
+	};
+    // return {
+    //     type: "EDIT_USER",
+    //     payload: {userId , editedUser}
+    // }
 };
 
 export const getAllUsers = () => {
 	return (dispatch, state, { getFirestore }) => {
 		getFirestore()
 			.collection("users")
+            .orderBy("timestamp", "desc")
 			.onSnapshot(
 				(snapshot) => {
 					let users = [];
 					snapshot.forEach((doc) => {
-						users.push(doc.data());
+						users.push({...doc.data(), id: doc.id});
 					});
 					console.log(users);
 					dispatch({ type: "SET_ALL_USERS", payload: users });
